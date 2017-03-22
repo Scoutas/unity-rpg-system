8477deff550348569b90b2d5f764ea9065adada0
Scoutas <aurimass110@gmail.com>
2017-03-22 12:43:33 +0200

    Initial Setup

---
+++ Assets/Scripts/Module.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace Editor{
+	public class Module {
+
+		public MainFrame Mainframe { get; private set;}
+
+		public virtual string Name {get;}
+		public virtual string Version {get;}
+
+		public virtual void Main()
+		{
+			
+		}
+
+		public bool SetMainframe(MainFrame mainframe){
+			if (Mainframe != null) {
+				Debug.LogError ("Mainframe parent of module " + Name + " has already been set. Could you possibly have several copies of a module?");
+				return false;
+			}
+			Mainframe = mainframe;
+			return true;
+		}
+			
+	}
+}
---
+++ Assets/Scripts/Resources/Modules/ItemSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Module{
+	
+	public class ItemSystem : Editor.Module {
+
+		public override string Name {
+			get {
+				return "Item System";
+			}
+		}
+
+		public override string Version {
+			get {
+				return "0.0.01";
+			}
+		}
+
+	
+		public override void Main ()
+		{
+			EditorGUILayout.LabelField ("This is an " + Name + " module!", EditorStyles.boldLabel);
+		}
+
+
+
+
+		
+	}
+}
